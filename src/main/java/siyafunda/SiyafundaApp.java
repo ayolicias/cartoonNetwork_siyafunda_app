0f1e09567139a2b8ccec63eacd617cf8bab2f626
Minenhle <ngwenyasiyamukela@gmail.com>
2019-12-10 16:14:28 +0200

    update merge

--- src/main/java/siyafunda/SiyafundaApp.java
+++ src/main/java/siyafunda/SiyafundaApp.java
@@ -16,31 +16,21 @@ public class SiyafundaApp {
         staticFiles.location("/public");
         port(8000);
 
-<<<<<<< HEAD
         try {
-            Connection connection =  DriverManager.getConnection("jdbc:postgresql://localhost/funda");
+            Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost/funda");
 
             get("/", (req, res) -> {
                 Map dataMap = new HashMap<>();
                 return new HandlebarsTemplateEngine().modelAndView(dataMap, "home.hbs");
             }, new HandlebarsTemplateEngine());
-        }catch (Exception e) {
+
+            get("/about", (req, res) -> {
+                Map dataMap = new HashMap<>();
+                return new HandlebarsTemplateEngine().modelAndView(dataMap, "About.hbs");
+            }, new HandlebarsTemplateEngine());
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
-=======
-        get("/", (req, res)-> {
-            Map dataMap = new HashMap<>();
-            return new HandlebarsTemplateEngine().modelAndView(dataMap, "index.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        get("/", (req, res)-> {
-            Map dataMap = new HashMap<>();
-            return new HandlebarsTemplateEngine().modelAndView(dataMap, "About.hbs");
-        }, new HandlebarsTemplateEngine());
     }
->>>>>>> ac160de99a575fbfecf03d5d1cb2bb29ef41c389
-    }
-
-
-
-
+}
